05e9ecf9d94b6a7305b17e5e46e7990345b24d94
Julian <Rijkenjulian@gmail.com>
2019-07-21 13:49:12 +0200

    scene organize

--- Kilmer_UnityProject/Assets/Content/Scripts/BusStop.cs
+++ Kilmer_UnityProject/Assets/Content/Scripts/BusStop.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class office : MonoBehaviour
+public class BusStop : MonoBehaviour
 {
 
     public GameUI gameUi;
